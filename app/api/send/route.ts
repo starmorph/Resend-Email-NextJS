2b92067e799290a048d046cf4cd98d49ae79cb27
starmorph <dylan@starmorph.com>
2024-01-08 10:58:24 -0700

    config email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -7,10 +7,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+      from: 'Dylan at Starmorph <onboarding@resend.dev>',
+      to: ['dylan@starmorph.com'],
+      subject: "Hello from Starmorph Resend",
+      react: EmailTemplate({ firstName: "Dylan" }) as React.ReactElement,
     });
 
     if (error) {
--- components/email-template.tsx
+++ components/email-template.tsx
@@ -9,6 +9,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
 }) => (
   <div>
     <h1>Welcome, {firstName}!</h1>
+    <p> This is an email sent with Resend.</p>
   </div>
 );
 
